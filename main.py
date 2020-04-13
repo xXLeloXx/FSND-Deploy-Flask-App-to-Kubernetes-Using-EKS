7313add89289aad89e688aaf057674faf16a3029
Leopold Kink <leopold.kink@live.de>
2020-04-13 21:34:42 +0200

    funktioniert nicht?

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def require_jwt(function):
 
 @APP.route('/', methods=['POST', 'GET'])
 def health():
-    return jsonify("Healthy_4")
+    return jsonify("Healthy_5")
 
 
 @APP.route('/auth', methods=['POST'])
