f11c47bc8a8e382bd81c805e95ab0b79d56c48a8
Leopold Kink <leopold.kink@live.de>
2020-04-13 13:29:18 +0200

    Test

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def require_jwt(function):
 
 @APP.route('/', methods=['POST', 'GET'])
 def health():
-    return jsonify("Healthy")
+    return jsonify("Healthy_test")
 
 
 @APP.route('/auth', methods=['POST'])
